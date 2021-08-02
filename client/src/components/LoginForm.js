5024710e797b447573eb57a176220f5a82a4e693
Susanne85 <xkqk2010@gmail.com>
2021-08-02 09:13:02 +0800

    Added login Form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,17 +3,18 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 
-import { loginUser } from '../utils/API';
+//import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 //Replace the `loginUser()` functionality imported from the `API` file with the `LOGIN_USER` mutation functionality.
-//import {LOGIN_USER} from '../utils/mutations';
+import {LOGIN_USER} from '../utils/mutations';
+console.log ('login', LOGIN_USER);
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  const [loginUser, {error,data}] = useMutation(LOGIN_USER);
+
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
-
-  //const [loginUser,{error}] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -31,7 +32,8 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const response = await loginUser({
+        variables:{ ...userFormData},});
 
       if (!response.ok) {
         throw new Error('something went wrong!');
